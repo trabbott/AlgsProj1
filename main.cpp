b25019c14489f5d55307a236f8043fac3951bd1a
grmoon <gregory.r.moon@gmail.com>
2016-02-04 02:51:19 -0700

    Added command line parsing

--- main.cpp
+++ main.cpp
@@ -6,12 +6,40 @@
 //  Copyright © 2016 Gregory Moon. All rights reserved.
 //
 
+#include <string>
+
 #include "FibHeap.hpp"
 #include "FileReader.hpp"
 #include "CommandLineReader.hpp"
 
+static void showUsage();
+static std::map<std::string, std::string> getParameters(int argc, char *argv[]);
+
 int main(int argc, char *argv[]){
-    /* 
+    /*
+     Command Line Parsing
+     
+     Would like to use getopt but it's not necessarily as cross-platform friendly as doing it a simpler/less effective way. The getParameters/showUsage functions should be fine for our purposes though.
+     */
+    /*
+    InputReaderResult *input;
+    
+    if(argc < 2){
+        input = CommandLineReader::read();
+    }
+    else{
+        std::map<std::string, std::string> params = getParameters(argc, argv);
+        
+        if(params["help"] == "t"){
+            showUsage();
+            return 0;
+        }
+        
+        input = FileReader::read(params["filepath"]);
+    }
+    */
+    
+    /*
      FibHeap Tests
      */
     
@@ -46,5 +74,50 @@ int main(int argc, char *argv[]){
     /*
      Command Line Reading Tests
      */
+    /*
     CommandLineReader::read();
+     */
+}
+
+static std::map<std::string, std::string> getParameters(int argc, char *argv[]){
+    std::map<std::string, std::string> options;
+    options["help"] = "f";
+    
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        
+        if ((arg == "-h") || (arg == "--help")) {
+            options["help"] = "t";
+        }
+        else if ((arg == "-f") || (arg == "--file")) {
+            if (i + 1 < argc) {
+                options["filepath"] = argv[++i];
+            }
+            else{
+                fprintf(stderr,"--file option requires one argument.\n");
+                options["help"] = "t";
+            }
+        }
+        else{
+            fprintf(stderr,"Unknown argument '%s'.\n", arg.c_str());
+            options["help"] = "t";
+        }
+        
+        if(options["help"] == "t"){
+            break;
+        }
+    }
+    
+    return options;
+}
+
+static void showUsage()
+{
+    fprintf(stderr,
+        "Usage: dijkstra <option(s)> INPUT\n"
+        "Options:\n"
+            "\t-h,--help\t\t\t\tShow this help message\n"
+            "\t-f,--file FILE_PATH\t\tRead input from a file\n\n"
+        "If neither -f nor --f is included, you will be prompted for inputs from the command line.\n\n"
+    );
 }
