3c60ae8643483cfb90d854cec402ab385499023d
grmoon <gregory.r.moon@gmail.com>
2016-02-12 14:18:12 -0700

    finished node comments

--- Node.cpp
+++ Node.cpp
@@ -7,6 +7,16 @@
 
 #include "Node.hpp"
 
+//============================================================================================
+// Node::Node
+//
+// Inputs:
+//  city: The City object to be contained within the node.s
+//
+// Outputs:
+//
+// The node constructor which initializes all attributes to their apropriate values.
+//============================================================================================
 Node::Node(City *city){
     this->item = city;
     this->rank = 0;
@@ -18,6 +28,17 @@ Node::Node(City *city){
     
 }
 
+//============================================================================================
+// Node::getChildren
+//
+// Inputs:
+//
+// Outputs:
+//  A vector of Node objects corresponding to the children of a node
+//
+//  A utility method that makes it easier to traverse through the children of a node without having
+//  to keep track of any pointers by using a vector.
+//============================================================================================
 std::vector<Node *> Node::getChildren(){
     std::vector<Node *> ret;
     Node *currNode;
@@ -34,6 +55,20 @@ std::vector<Node *> Node::getChildren(){
     return ret;
 }
 
+//============================================================================================
+// Node::link
+//
+// Inputs:
+//  first:  The first of the Node objects to be linked.
+//  second: The other Node object to be linked.
+//
+// Outputs:
+//  The Node with the smaller distance of the two that were input.
+//
+// This methos is analagous to the FibHeap _meld method. Two nodes are combined so that the one
+// whose distance is larger becomes a child of the one whose distance is smaller. The combindation
+// is done in such a way that the mean heap property of each node is maintained.
+//============================================================================================
 Node *Node::link(Node *first, Node *second){
     Node *a, *b;
     
@@ -60,6 +95,17 @@ Node *Node::link(Node *first, Node *second){
     return a;
 }
 
+//============================================================================================
+// Node::addChild
+//
+// Inputs:
+//  newChild: The Node object to be added as a child to the current Node.
+//
+// Outputs:
+//
+// Add a child to the current node. After the new node is added, the _linkChildren method is called
+// which ensures that the min heap properties are maintained by every node.
+//============================================================================================
 void Node::addChild(Node *newChild){
     if (newChild == nullptr) {
         return;
@@ -84,6 +130,16 @@ void Node::addChild(Node *newChild){
     this->_linkChildren();
 }
 
+//============================================================================================
+// Node::_linkChildren
+//
+// Inputs:
+//
+// Outputs:
+//
+// Analagous to the link method of the FibHeap class. All of the children with the same # of
+// children are combined to maintain the required min heap properties of each node.
+//============================================================================================
 void Node::_linkChildren(){
     bool update;
     std::map<int, Node *> rankMap;
@@ -113,12 +169,28 @@ void Node::_linkChildren(){
     }
 }
 
+//============================================================================================
+// Node::distance
+//
+// Inputs:
+//
+// Outputs:
+//  The distance attribute of the City object contained within the node.
+//============================================================================================
 unsigned long Node::distance(){
    // return std::max(this->item->opt->value, this->item->mand->value);
     
     return this->item->distance;
 }
 
+//============================================================================================
+// Node::key
+//
+// Inputs:
+//
+// Outputs:
+//  The id attribute of the City object contained within the node.
+//============================================================================================
 unsigned long Node::key(){
     return this->item->key;
 }
--- Node.hpp
+++ Node.hpp
@@ -13,7 +13,16 @@
 
 #include "City.hpp"
 
-
+//============================================================================================
+// Node
+//
+// A simple class that acts as a node in a FibHeap as well as methods to help the FibHeap
+// stay organized properly. It contains pointers to its parent, child, left neightbor, and right
+// neighbor to facilitate the creation of doubly-linked lists which allow the formation of
+// circular lists which let FibHeap operations run with the desired time complexities. It also
+// keeps track of its rank (# of children) and whether or not it has been marked (cut from its
+// parent).
+//============================================================================================
 class Node{
 public:
     Node *parent, *child, *left, *right;
