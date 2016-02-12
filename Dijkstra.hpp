a0a696406d0e3beb3346df52a05b56d3fca91d79
grmoon <gregory.r.moon@gmail.com>
2016-02-12 11:01:58 -0700

    Commented Dijktra's files

--- Dijkstra.hpp
+++ Dijkstra.hpp
@@ -7,6 +7,12 @@
 #include "FibHeap.hpp"
 #include "InputReader.hpp"
 
+//============================================================================================
+// DijkstraResult
+//
+// A simple struct used to hold and return the results of Dijkstra's Algorithm.
+//============================================================================================
+
 struct DijkstraResult{
     std::map<unsigned long, City *> previous;
     std::map<unsigned long, unsigned long> distances;
@@ -14,13 +20,20 @@ struct DijkstraResult{
     DijkstraResult(std::map<unsigned long, City *> previous, std::map<unsigned long, unsigned long> distances);
 };
 
-class Dijkstra{
 
+//============================================================================================
+// Dijkstra
+//
+// An abstract class used to run Dijkstra's Algorithm either forward or backward.
+//============================================================================================
+
+class Dijkstra{
 public:
     static DijkstraResult *runForward(InputReaderResult *params);
     static DijkstraResult *runBackward(InputReaderResult *params);
     //static DijkstraResult *run(InputReaderResult *params, bool forward);
-    static DijkstraResult *run(unsigned long from, unsigned long to, std::map<unsigned long, City *> cities, bool forward);
+private:
+    static DijkstraResult *_run(unsigned long from, unsigned long to, std::map<unsigned long, City *> cities, bool forward);
 };
 
 #endif /* Dijkstra_hpp */
